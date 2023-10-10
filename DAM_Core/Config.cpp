76966526c0cd4c3c08b464ce76e4fdc3115f86a9
DartRuffian <skulldude327@gmail.com>
2023-10-09 23:39:19 -0500

    Core patch/mod

---
+++ DAM_Core/Config.cpp
@@ -0,0 +1,26 @@
+class CfgMods
+{
+    class Mod_Base;
+    class BNA_KC: Mod_Base
+    {
+        // logo = "";
+        // logoOver = "";
+        // picture = "";
+        name = "Dart Armory - Memes";
+        dlcColor[] = {0, 0, 0, 1};
+    };
+};
+
+
+class CfgPatches
+{
+    class DAM_Core
+    {
+        author = "DartRuffian";
+        name = "Dart Armory - Memes"
+        requiredVersion = 1;
+        requiredAddons[] = {};
+        units[] = {};
+        weapons[] = {};
+    };
+};
