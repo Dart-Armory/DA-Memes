c2498139565026d1e0389239d63fca6667c00177
DartRuffian <skulldude327@gmail.com>
2023-10-09 23:24:24 -0500

    AKM mag

--- DAM_Weapons/SpaceAK/Config.cpp
+++ DAM_Weapons/SpaceAK/Config.cpp
@@ -21,3 +21,24 @@ class CfgWeapons
         magazineWell[] = {};
     };
 };
+
+
+class CfgMagazines
+{
+    class Aux501_Weapons_Mags_20mwdp30;
+    class DAM_Mag_AKM_30Rnd: Aux501_Weapons_Mags_20mwdp30
+    {
+        author = "DartRuffian";
+        displayName = "[DA] AKM Magazine";
+        displayNameShort = "Medium Energy";
+        descriptionShort = "Energy Cell<br/>Rounds: 30<br/>Used In: AKM";
+        count = 30;
+
+        JLTS_hasEMPProtection = 1;
+
+        model = "\A3\weapons_F\ammo\mag_univ.p3d";
+        modelSpecial = "\a3\Weapons_F_Exp\MagazineProxies\mag_762x39_akm_30rnd.p3d";
+        modelSpecialIsProxy = 1;
+        picture = "\A3\Weapons_F_Exp\Data\UI\icon_30Rnd_762x39_Mag_F_ca.paa";
+    };
+};
